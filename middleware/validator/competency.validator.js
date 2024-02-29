349fab896f5fb759a8ba5ad31b38d37991b8a158
yasmeenTarekKhater <yasmeentarek112001@gmail.com>
2024-03-01 00:37:44 +0200

    update competency model
    add category and teamed assign in model ,handle routes

--- middleware/validator/competency.validator.js
+++ middleware/validator/competency.validator.js
@@ -5,4 +5,6 @@ module.exports.addCompetency = joi.object().required().keys({
     name: joi.string().min(3).max(100).required(),
     defaultDescription: joi.string(),
     seniorityLevels: joi.array(),
+    category:joi.string(),
+    teamsAssigned:joi.array().items(joi.string())
 })
