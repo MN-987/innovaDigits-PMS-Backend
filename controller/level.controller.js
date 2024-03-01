edf94e0b062d0a1f5475174fdc925784c515c453
Mostafa Nasser <mostafanasserm98@gmail.com>
2024-03-01 04:19:44 +0200

    Update level.controller.js

--- controller/level.controller.js
+++ controller/level.controller.js
@@ -1,53 +1,47 @@
+const levelService = require('../service/level.service.js');
+const ErrorClass = require('../util/errorClass.js');
 
-const levelService =require('../service/level.service.js')
+module.exports.getAllLevels = async (req, res) => {
+    const levels = await levelService.getAllLevelsServices();
+    res.status(200).json({ status: "success", data: { levels } });
+}
 
-module.exports.getAllLevels =async (req,res) => {
-        await levelService.getAllLevelsServices().then((levels)=>{
-            res.status(200).json({ status: "success", data: { levels } })
-        })
-      
+module.exports.getLevelById = async (req, res, next) => {
+    const levelID = req.params.levelID;
+    const level = await levelService.getLevelByIdServices(levelID);
+    if (!level) {
+        return next(new ErrorClass('This level is not found', 404));
+    }
+    res.status(200).json({ status: "success", data: { level } });
+}
+
+module.exports.updateLevel = async (req, res, next) => {
+    const levelID = req.params.levelID;
+    const updatedData = req.body;
+    const level = await levelService.updateLevelServices(levelID, updatedData);
+    if (level.modifiedCount === 0) {
+        return next(new ErrorClass('This level is not found.', 404));
     }
 
-module.exports.getLevelById = async (req,res, next) => {
-        const levelID = req.params.levelID;
-         await levelService.getLevelByIdServices(levelID).then((level)=>{
-          
-            return res.status(200).json({ status: "success", data: { level } })
-        }).catch((error)=>{
-            next(error)
-        })
-        
+    res.status(200).json({ status: "success", data: { level } });
+}
+
+
+module.exports.addNewLevel = async (req, res, next) => {
+
+    const existingLevel = await levelService.getLevelByName(req.body.levelName);
+    if (existingLevel) {
+        return  next(new ErrorClass('Level name already exists.',400))
     }
+    const level = await levelService.addLevelServices(req.body);
+    res.status(201).json({ status: "success", data: { levels: level } });
+}
 
-
-module.exports.updateLevel =async (req,res, next) => {
-      
-        const levelId = req.params.levelID;
-        let updatedData = req.body
-        await levelService.updateLevelServices(levelId, updatedData).then((level)=>{
-        res.status(200).json({ status: "success" ,data:{level} })
-       })
-         
+module.exports.deleteLevel = async (req, res, next) => {
+    const levelID = req.params.levelID;
+    const deletedLevel = await levelService.deleteLevelServices(levelID);
+    if (deletedLevel.deletedCount === 0) {
+        return next(new ErrorClass('This level is not found', 404));
     }
-
-module.exports.addNewLevel = async(req,res,next)=>{
-    
-    
-       await levelService.addLevelServices(req.body).then((level)=>{
-        res.status(201).json({ status:"success", data: {levels: level } });
-       })
-
-    }
- 
-
- module.exports.deleteLevel = async(req,res,next)=>{
-     const levelID =req.params.levelID;
-     levelService.deleteLevelServices(levelID).then((level)=>{
-        res.status(200).json({ status:"success", data:null});
-
-     })
-
-    }
-
-
-
+    res.status(200).json({ status: "success", data: null });
+}
