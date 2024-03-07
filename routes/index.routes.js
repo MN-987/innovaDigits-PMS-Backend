879fcf1a68dc4d0829b7998019d30d8413e66662
Hamas Ahmed <hamasahmed0@gmail.com>
2024-03-08 00:01:22 +0200

    add pagination in index.routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -11,6 +11,7 @@ const express = require('express');
 const userRoutes = require("../routes/user.routes.js");
 const cors = require('cors');
 const Competency = require("../data/competency.model.js");
+const Feedback = require('../data/feedback.model.js')
 const bootstrap = (app, express) => {
 
     //register middlewares 
@@ -46,7 +47,39 @@ const bootstrap = (app, express) => {
     app.use("/api/v1/competency", competencyRouter)
     app.use("/api/v1/auth", authRouter);
 
+    app.get('/pagination', async (req, res) => {
+        try {
+            // Parse pagination parameters from the request query
+            const page = parseInt(req.query.page) || 1;
+            const pageSize = parseInt(req.query.pageSize) || 3;
 
+            // Calculate skip count based on pagination parameters
+            const skip = (page - 1) * pageSize;
+
+            // Fetch paginated competencies from the database
+            const competencies = await Feedback.find()
+                .skip(skip)
+                .limit(pageSize)
+                .exec();
+
+            // Count total number of competencies in the database
+            const totalCompetencies = await Feedback.countDocuments();
+
+            // Calculate total number of pages
+            const totalPages = Math.ceil(totalCompetencies / pageSize);
+
+            res.json({
+                currentPage: page,
+                totalPages: totalPages,
+                pageSize: pageSize,
+                totalCount: totalCompetencies,
+                data: competencies,
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    });
 
     app.use("*", (req, res, next) => {
         return res.json({ message: "In-valid Routing" });
